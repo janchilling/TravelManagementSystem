71405e60e369afcf22b07e70606790e13896291f
Keshan <janindupathi@gmail.com>
2023-12-17 01:50:08 +0530

    Changed the folder structure and added the frontend

---
+++ springboot-backend/src/main/java/com/travelmanagement/travelsystem/entities/Role.java
@@ -0,0 +1,7 @@
+package com.travelmanagement.travelsystem.entities;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
--- src/main/java/com/travelmanagement/travelsystem/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.travelmanagement.travelsystem.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-
-    UserDetailsService userDetailsService();
-}
